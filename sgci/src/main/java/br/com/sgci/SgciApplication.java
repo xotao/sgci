233926647ca297b516a85b854936fe18e79216a0
Rafael Previdi <henriqueprevidirafael@unimil.com.br>
2026-04-14 22:07:50 -0300

    inclusao codigo fonte inicial

---
+++ sgci/src/main/java/br/com/sgci/SgciApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgciApplication.class, args);
+	}
+
+}
---
+++ sgci/src/test/java/br/com/sgci/SgciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
